a1ee650abad2ddd5b431eff46baafeba62e54ea8
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-15 02:09:05 +0200

    updated header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import { FlexContainer } from '../FlexContainer'
 
+const menuConfig = [
+    'The Challenge',
+    'The Fix',
+    'How it works',
+    'Benefits',
+    'Comparison',
+]
+
 export const Header = () => {
     return (
-        <header className="fixed top-0 left-0 w-full px-5 bg-[#0a0a0a] z-50">
+        <header className="fixed top-0 left-0 w-full px-5 bg-[#0a0a0a] z-50 font-roboto-mono">
             <FlexContainer
                 justifyContent="justify-between"
                 alignItems="items-center"
-                className="border-b-[0.5px] border-blue-100 py-2.5"
+                className="border-b-[0.5px] border-[#363E44] py-2.5"
             >
                 <div>Logo</div>
                 <nav>
-                    <ul className="flex gap-2">
-                        <li>The Challenge</li>
-                        <li>The Fix</li>
-                        <li>How it works</li>
-                        <li>Benefits</li>
-                        <li>Comparison</li>
+                    <ul className="flex gap-4">
+                        {menuConfig.map((link) => (
+                            <li key={link} className="cursor-pointer">
+                                {link}
+                            </li>
+                        ))}
                     </ul>
                 </nav>
-                <button className="py-2.5 px-4 bg-[#0DBFBB] uppercase">
+                <button className="py-2.5 px-4 bg-gradient-to-r text-[15px] from-[#0DBFBB33] to-[#0F8D8C33] text-[#6DE1CE] uppercase cursor-pointer">
                     Free demo
                 </button>
             </FlexContainer>
