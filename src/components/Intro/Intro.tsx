adb83ef94a8cc289c32f6b8bf34ca39177c30055
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-17 02:41:51 +0200

    update Intro section

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -5,6 +5,7 @@ import { P } from '../Typography/P'
 import { Button } from '../Button'
 import Image from 'next/image'
 import intro from '@/assets/intro.svg'
+import videoPreview from '@/assets/videoPreview.png'
 
 export const Intro = () => {
     return (
@@ -13,14 +14,14 @@ export const Intro = () => {
                 direction="flex-col"
                 justifyContent="justify-center"
                 gap="gap-10"
-                className="relative px-10 border-r-[0.5px] border-l-[0.5px] border-b-[0.5px] border-[#363E44] flex-1"
+                className="relative pl-14 border-r-[0.5px] border-l-[0.5px] border-b-[0.5px] border-[#363E44] flex-1"
             >
                 <div className="animate-move-y-left" />
                 <div className="animate-move-y-right" />
                 <H1>
-                    Cadbury is an AI <br />
+                    Cadbury is an <br />
                     <span className="bg-gradient-to-r from-[#6DE1CE] via-[#288FF6] to-[#32FFFF] bg-clip-text text-transparent">
-                        Powered NetSuite
+                        AI NetSuite
                     </span>
                     <br />
                     Consultant
@@ -43,7 +44,7 @@ export const Intro = () => {
                         consultant you’ll ever need.
                     </P>
                 </FlexContainer>
-                <Button>Schedule a Consultation</Button>
+                <Button className="mt-8">Schedule a Consultation</Button>
             </FlexContainer>
 
             <FlexContainer
@@ -55,6 +56,9 @@ export const Intro = () => {
             </FlexContainer>
 
             <div className="animate-move-x-bottom" />
+            <div className="absolute right-0 bottom-0 w-48">
+                <Image src={videoPreview} alt="preview video" />
+            </div>
         </section>
     )
 }
