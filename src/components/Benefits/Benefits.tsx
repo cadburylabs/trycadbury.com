acdb5950baa31376b3aaa8fe942906097a664fda
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-18 11:41:04 +0200

    updated benefits section

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -3,12 +3,28 @@ import { FlexContainer } from '../FlexContainer'
 import { H2 } from '../Typography/H2'
 import { H3 } from '../Typography/H3'
 import { P } from '../Typography/P'
+import Image from 'next/image'
+import visual from '@/assets/visual.png'
 
 export const Benefits = () => {
     return (
-        <section className="flex mx-5 flex-col">
-            <FlexContainer className="border-b-[0.5px] border-l-[0.5px] border-r-[0.5px] border-[#363E44]">
-                <H2>Benefits</H2>
+        <section className="relative flex mx-5 flex-col">
+            <div className="animate-move-y-left" />
+            <div className="animate-move-y-right" />
+            <FlexContainer className="relative px-14 py-10 border-b-[0.5px] border-l-[0.5px] border-r-[0.5px] border-[#363E44]">
+                <FlexContainer
+                    justifyContent="justify-between"
+                    alignItems="items-center"
+                >
+                    <H2>Benefits</H2>
+                    <span className="uppercase">Value</span>
+                </FlexContainer>
+                <FlexContainer
+                    justifyContent="justify-end"
+                    alignItems="items-center"
+                >
+                    <span>/04</span>
+                </FlexContainer>
             </FlexContainer>
             <FlexContainer
                 direction="flex-row"
@@ -16,25 +32,36 @@ export const Benefits = () => {
             >
                 <FlexContainer alignItems="items-center">
                     <FlexContainer
+                        width="w-fit"
                         direction="flex-col"
                         justifyContent="justify-between"
-                        className="flex-[0] pr-20"
+                        className="pl-20 py-12 h-full"
                     >
                         <P>BS-01</P>
-                        <P className="uppercase">Endless</P>
+                        <P className="uppercase">Swift</P>
                     </FlexContainer>
-                    <div className="px-20">Logo</div>
-                    <FlexContainer direction="flex-col">
+                    <Image
+                        src={visual}
+                        alt="visual animation"
+                        className="mr-10"
+                    />
+                    <FlexContainer
+                        width="max-w-[450px]"
+                        gap="gap-[30px]"
+                        direction="flex-col"
+                    >
                         <H3>Speed</H3>
                         <P>
-                            NetSuite is a deep product that spans many modules
-                            and add-ons. Day by day, we are mapping these
-                            corners so that Cadbury becomes the AI powered
-                            NetSuite consultant for everyone.
+                            Cadbury takes tasks that would take a human
+                            consultant days or weeks to finish—and leverages AI
+                            to complete them in minutes. We believe your
+                            business deserves to move fast, and shouldn’t be
+                            slowed down by inefficient consulting firms.
                         </P>
                     </FlexContainer>
                 </FlexContainer>
             </FlexContainer>
+            <div className="animate-move-x-bottom" />
         </section>
     )
 }
