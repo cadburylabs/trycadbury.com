8d0fff8eddece25cfbbb33cb9cba86007b28341e
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-23 03:19:51 +0200

    apply lenis scroll to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto_Mono } from 'next/font/google'
 import './globals.css'
+import LenisRoot from '@/hooks/useLenisScroller'
 
 const robotoMono = Roboto_Mono({
     subsets: ['latin'],
@@ -20,6 +21,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`antialiased ${robotoMono.variable}`}>
+                <LenisRoot />
                 {children}
             </body>
         </html>
