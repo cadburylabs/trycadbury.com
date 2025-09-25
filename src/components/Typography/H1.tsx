d3103cb359261042a2f4d81bd1714f0389c7a2cd
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-26 01:03:29 +0200

    responsive h1

--- src/components/Typography/H1.tsx
+++ src/components/Typography/H1.tsx
@@ -7,15 +7,20 @@ export const H1 = forwardRef<
         fontSize?: string
         className?: string
     }
->(({ children, fontSize = 'text-5xl md:text-8xl', className }, ref) => {
-    return (
-        <h1
-            ref={ref}
-            className={`${className} ${fontSize} font-medium scroll-m-20 tracking-tight`}
-        >
-            {children}
-        </h1>
-    )
-})
+>(
+    (
+        { children, fontSize = 'text-5xl lg:text-7xl xl:text-8xl', className },
+        ref
+    ) => {
+        return (
+            <h1
+                ref={ref}
+                className={`${className} ${fontSize} font-medium scroll-m-20 tracking-tight`}
+            >
+                {children}
+            </h1>
+        )
+    }
+)
 
 H1.displayName = 'H1'
