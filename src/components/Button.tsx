118ab2abaf9509d03ea14913ca788dbb9b0d76f3
vadym.kostucok <vadym.kostucok@gmail.com>
2025-09-26 10:54:12 +0200

    open form on button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Image from 'next/image'
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 import buttonArrow from '@/assets/buttonArrow.svg'
+import { Form } from './Contact/Form'
 
 export const Button = ({
     children,
@@ -9,16 +11,22 @@ export const Button = ({
     children: ReactNode
     className?: string
 }) => {
+    const [showForm, setShowForm] = useState(false)
+
     return (
-        <button
-            className={`${className} w-full lg:w-fit p-5 lg:p-[30px] flex gap-1.5 justify-center lg:justify-start rounded-lg button-background uppercase text-[15px] lg:text-lg font-medium font-roboto-mono cursor-pointer`}
-        >
-            {children}
-            <Image
-                src={buttonArrow}
-                alt="button arrow"
-                className="inline-block"
-            />
-        </button>
+        <>
+            <button
+                className={`${className} w-full lg:w-fit p-5 lg:p-[30px] flex gap-1.5 justify-center lg:justify-start rounded-lg button-background uppercase text-[15px] lg:text-lg font-medium font-roboto-mono cursor-pointer`}
+                onClick={() => setShowForm(true)}
+            >
+                {children}
+                <Image
+                    src={buttonArrow}
+                    alt="button arrow"
+                    className="inline-block"
+                />
+            </button>
+            <Form open={showForm} onClose={() => setShowForm(false)} />
+        </>
     )
 }
