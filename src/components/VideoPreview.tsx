b0067a9733f2132cb79e867d89730e6157c1472f
Manish Sinha <arithmetic@gmail.com>
2025-09-30 11:44:49 -0700

    wip

--- src/components/VideoPreview.tsx
+++ src/components/VideoPreview.tsx
@@ -34,16 +34,19 @@ export const VideoPreview = ({
             } my-8 group cursor-pointer relative`}
             onClick={handleClick}
         >
-            <Image
-                src={src}
-                alt={alt}
-                className="group-hover:opacity-85 duration-300"
-            />
+            <div className="relative border-2 border-[#6DE1CE] rounded-lg overflow-hidden">
+                <Image
+                    src={src}
+                    alt={alt}
+                    className="duration-300"
+                />
 
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[62px] h-[62px] rounded-full bg-gradient-to-b from-white/20 backdrop-blur-sm to-transparent z-0" />
+                {/* Enhanced play button with glow effect */}
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[80px] h-[80px] rounded-full bg-gradient-to-b from-white/30 backdrop-blur-sm to-transparent z-0" />
 
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[60px] h-[60px] rounded-full bg-black z-10 opacity-60 group-hover:opacity-70 backdrop-blur-sm transition duration-300">
-                <div className="w-0 h-0 border-t-[8px] border-t-transparent border-b-[8px] border-b-transparent border-l-[12px] border-l-white ml-1" />
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-[76px] h-[76px] rounded-full bg-black/80 z-10 opacity-70 backdrop-blur-sm">
+                    <div className="w-0 h-0 border-t-[10px] border-t-transparent border-b-[10px] border-b-transparent border-l-[16px] border-l-white ml-1" />
+                </div>
             </div>
         </Box>
     )
